2088ea8ed81e1c1672ec23ef7fae276639c2694a
sidhujag <sidhujag@hotmail.com>
2015-12-11 11:52:17 -0800

    fix default genesis amount

--- src/main.cpp
+++ src/main.cpp
@@ -1566,9 +1566,9 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     nHeight += 700000;
     CAmount nSubsidy = 96 * COIN;
 	if(nHeight == 700000)
-		nSubsidy = 1024 * COIN; // SYSCOIN genesis amount on shade
+		nSubsidy = 50 * COIN; // SYSCOIN genesis amount on shade
 	else if(nHeight == 700001)
-		nSubsidy = 439724938 * COIN; // SYSCOIN shade snapshot amount to old syscoin blockchain
+		nSubsidy = 439725912 * COIN; // SYSCOIN shade snapshot amount to old syscoin blockchain
     else if(nHeight > 777840 && nHeight <= 1814640)
         nSubsidy = 80 * COIN;
     else if(nHeight > 1814640 && nHeight <= 3369840)
