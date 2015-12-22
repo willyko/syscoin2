ae69473774d5a951b059cc6064c478687f38d2d2
sidhujag <sidhujag@hotmail.com>
2015-12-22 13:01:27 -0800

    create first syscoin test

--- src/test/syscoin_tests.cpp
+++ src/test/syscoin_tests.cpp
@@ -4,9 +4,16 @@
 
 BOOST_FIXTURE_TEST_SUITE (syscoin_tests, SyscoinTestingSetup)
 
-BOOST_AUTO_TEST_CASE (alias_tests)
+BOOST_AUTO_TEST_CASE (generate_200blocks)
 {
-  
+  CallRPC("node1", "generate 200");
+  UniValue r;
+  r = CallRPC("node1", "getinfo");
+  BOOST_CHECK(find_value(r.get_obj(), "blocks").get_int() == 200);
+  r = CallRPC("node2", "getinfo");
+  BOOST_CHECK(find_value(r.get_obj(), "blocks").get_int() == 200);
+  r = CallRPC("node3", "getinfo");
+  BOOST_CHECK(find_value(r.get_obj(), "blocks").get_int() == 200);
 }
 
 
