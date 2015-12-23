24877e0319448868411c7964ac98e862f79fb7fc
sidhujag <sidhujag@hotmail.com>
2015-12-22 16:04:04 -0800

    global testcase

--- src/test/syscoin_tests.cpp
+++ src/test/syscoin_tests.cpp
@@ -1,9 +1,7 @@
 #include "test/test_syscoin_services.h"
 #include "utiltime.h"
 #include <boost/test/unit_test.hpp>
-
-BOOST_FIXTURE_TEST_SUITE (syscoin_tests, SyscoinTestingSetup)
-
+BOOST_GLOBAL_FIXTURE( SyscoinTestingSetup )
 BOOST_AUTO_TEST_CASE (generate_sysrates_alias)
 {
 	GenerateBlocks(200);
