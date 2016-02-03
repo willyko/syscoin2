6cdb828666b676221b4993e0b41160b133208565
sidhujag <sidhujag@hotmail.com>
2016-02-03 12:28:21 -0800

    compile

--- src/message.cpp
+++ src/message.cpp
@@ -292,26 +292,22 @@ bool CheckMessageInputs(const CTransaction &tx,
 		{
 			return error("message data from too big");
 		}
-		switch (op) {
-		case OP_MESSAGE_ACTIVATE:
-			vector<CAliasIndex> vtxMyPos;
+		if(op == OP_MESSAGE_ACTIVATE)
+		{
+			vector<CAliasIndex> vtxPos;
 			if(!IsAliasOp(prevAliasOp))
 				return error("CheckMessageInputs(): alias not provided as input");
-			if (!paliasdb->ReadAlias(vvchPrevAliasArgs[0], vtxMyPos))
+			if (!paliasdb->ReadAlias(vvchPrevAliasArgs[0], vtxMyPosvtxPos)
 				throw runtime_error("CheckMessageInputs(): failed to read alias from alias DB");
-			if (vtxMyPos.size() < 1)
+			if (vtxPos.size() < 1)
 				throw runtime_error("CheckMessageInputs(): no alias result returned");
-			if(vtxMyPos.back().vchPubKey != theMessage.vchPubKeyFrom)
+			if(vtxPos.back().vchPubKey != theMessage.vchPubKeyFrom)
 				return error("CheckMessageInputs() OP_MESSAGE_ACTIVATE: alias and message from pubkey's must match");
 			break;
-
-		default:
-			return error( "CheckMessageInputs() : message transaction has unknown op");
 		}
-	
-
-
-
+		else
+			return error( "CheckMessageInputs() : message transaction has unknown op");
+		
         // these ifs are problably total bullshit except for the messagenew
         if (fBlock || (!fBlock && !fMiner && !fJustCheck)) {
 			// save serialized message for later use
