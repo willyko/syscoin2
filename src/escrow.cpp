a1e871b04ee97277fcd08647b4391716820dc8d6
sidhujag <sidhujag@hotmail.com>
2016-02-09 15:33:20 -0800

    fix escrow release

--- src/escrow.cpp
+++ src/escrow.cpp
@@ -351,7 +351,7 @@ bool CheckEscrowInputs(const CTransaction &tx,
 			case OP_ESCROW_RELEASE:
 				if(theEscrow.IsNull() && prevOp != OP_ESCROW_RELEASE)
 					return error("CheckEscrowInputs() : null escrow must have release escrow input");
-				else if(prevOp != OP_ESCROW_ACTIVATE)
+				if(!theEscrow.IsNull() && prevOp != OP_ESCROW_ACTIVATE)
 					return error("CheckEscrowInputs() : can only release an activated escrow");
 				// Check input
 				if (vvchPrevArgs[0] != vvchArgs[0])
