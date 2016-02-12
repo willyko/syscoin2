27748d38137f4e98c720d71d8b3448cf3cf2b37e
sidhujag <sidhujag@hotmail.com>
2016-02-12 12:05:21 -0800

    typo

--- src/offer.cpp
+++ src/offer.cpp
@@ -911,7 +911,7 @@ bool CheckOfferInputs(const CTransaction &tx,
 						if(!GetTxOfOffer(*pofferdb, theOffer.vchLinkOffer, linkOffer, linkedTx))
 							return error("CheckOfferInputs() OP_OFFER_ACCEPT: could not get linked offer");
 					}
-					if(theOfferAccept.nQty <= 0 || (theOffer.nQty != -1 && theOfferAccept.nQty > theOffer.nQty) || (!linkOffer.IsNull() && theOfferAccept.nQty > linkOffer.nQty && linkOffer.nQty != -1)))
+					if(theOfferAccept.nQty <= 0 || (theOffer.nQty != -1 && theOfferAccept.nQty > theOffer.nQty) || (!linkOffer.IsNull() && theOfferAccept.nQty > linkOffer.nQty && linkOffer.nQty != -1))
 						return error("CheckOfferInputs() OP_OFFER_ACCEPT: txn %s rejected because desired qty %u is more than available qty %u\n", tx.GetHash().GetHex().c_str(), theOfferAccept.nQty, theOffer.nQty);
 				}
 				break;
