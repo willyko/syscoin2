339a26947ff057e8e6ec6f20d06228346795b10e
sidhujag <sidhujag@hotmail.com>
2016-02-22 14:57:27 -0800

    offerlink dialog

--- src/qt/resellofferdialog.cpp
+++ src/qt/resellofferdialog.cpp
@@ -20,7 +20,7 @@ ResellOfferDialog::ResellOfferDialog(QModelIndex *idx, QWidget *parent) :
 	ui->descriptionEdit->setPlainText(idx->data(OfferTableModel::DescriptionRole).toString());
 	ui->offerGUIDLabel->setText(offerGUID);
 	ui->commissionDisclaimer->setText(tr("<font color='red'>The payment of <b>commission</b> for an offer sale. Payments will be calculated on the basis of a percentage of the offer value. Enter your desired percentage.</font>"));
-
+	loadAliases();
 }
 
 ResellOfferDialog::~ResellOfferDialog()
@@ -35,6 +35,7 @@ bool ResellOfferDialog::saveCurrentRow()
 	string strMethod;
 
 	strMethod = string("offerlink");
+	params.push_back(ui->aliasEdit->currentText().toStdString());
 	params.push_back(ui->offerGUIDLabel->text().toStdString());
 	params.push_back(ui->commissionEdit->text().toStdString());
 	params.push_back(ui->descriptionEdit->toPlainText().toStdString());
@@ -52,19 +53,80 @@ bool ResellOfferDialog::saveCurrentRow()
 	{
 		string strError = find_value(objError, "message").get_str();
 		QMessageBox::critical(this, windowTitle(),
-		tr("Error creating new whitelist entry: \"%1\"").arg(QString::fromStdString(strError)),
+		tr("Error creating new linked offer: \"%1\"").arg(QString::fromStdString(strError)),
 			QMessageBox::Ok, QMessageBox::Ok);
 	}
 	catch(std::exception& e)
 	{
 		QMessageBox::critical(this, windowTitle(),
-			tr("General exception creating new whitelist entry: \"%1\"").arg(QString::fromStdString(e.what())),
+			tr("General exception creating new linked offer: \"%1\"").arg(QString::fromStdString(e.what())),
 			QMessageBox::Ok, QMessageBox::Ok);
 	}							
 
     return false;
 }
+void ResellOfferDialog::loadAliases()
+{
+	ui->aliasEdit->clear();
+	string strMethod = string("aliaslist");
+    UniValue params(UniValue::VARR); 
+	UniValue result ;
+	string name_str;
+	int expired = 0;
+	
+	try {
+		result = tableRPC.execute(strMethod, params);
 
+		if (result.type() == UniValue::VARR)
+		{
+			name_str = "";
+			expired = 0;
+
+
+	
+			const UniValue &arr = result.get_array();
+		    for (unsigned int idx = 0; idx < arr.size(); idx++) {
+			    const UniValue& input = arr[idx];
+				if (input.type() != UniValue::VOBJ)
+					continue;
+				const UniValue& o = input.get_obj();
+				name_str = "";
+
+				expired = 0;
+
+
+		
+				const UniValue& name_value = find_value(o, "name");
+				if (name_value.type() == UniValue::VSTR)
+					name_str = name_value.get_str();		
+				const UniValue& expired_value = find_value(o, "expired");
+				if (expired_value.type() == UniValue::VNUM)
+					expired = expired_value.get_int();
+				
+				if(expired == 0)
+				{
+					QString name = QString::fromStdString(name_str);
+					ui->aliasEdit->addItem(name);					
+				}
+				
+			}
+		}
+	}
+	catch (UniValue& objError)
+	{
+		string strError = find_value(objError, "message").get_str();
+		QMessageBox::critical(this, windowTitle(),
+			tr("Could not refresh cert list: %1").arg(QString::fromStdString(strError)),
+				QMessageBox::Ok, QMessageBox::Ok);
+	}
+	catch(std::exception& e)
+	{
+		QMessageBox::critical(this, windowTitle(),
+			tr("There was an exception trying to refresh the cert list: ") + QString::fromStdString(e.what()),
+				QMessageBox::Ok, QMessageBox::Ok);
+	}         
+ 
+}
 void ResellOfferDialog::accept()
 {
     
--- src/qt/resellofferdialog.h
+++ src/qt/resellofferdialog.h
@@ -20,7 +20,7 @@ class ResellOfferDialog : public QDialog
 public:
     explicit ResellOfferDialog(QModelIndex *idx, QWidget *parent = 0);
     ~ResellOfferDialog();
-
+	void loadAliases();
 public Q_SLOTS:
     void accept();
 
