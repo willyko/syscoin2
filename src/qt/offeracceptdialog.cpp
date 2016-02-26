bef6a69a08034713e3a32e6379b28ecd95a660fb
sidhujag <sidhujag@hotmail.com>
2016-02-26 15:20:49 -0800

    typo

--- src/qt/offeracceptdialog.cpp
+++ src/qt/offeracceptdialog.cpp
@@ -35,7 +35,7 @@ OfferAcceptDialog::OfferAcceptDialog(const PlatformStyle *platformStyle, QString
 		ui->acceptBtcButton->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/send"));
 		ui->cancelButton->setIcon(platformStyle->SingleColorIcon(":/icons/" + theme + "/quit"));
 	}
-	ui->aboutShade->setPixmap(QPixmap(QString::fromUtf8(":/icons/" + theme.toStdString().c_str() + "/about")));
+	ui->aboutShade->setPixmap(QPixmap(QString::fromUtf8(strprintf(":/icons/" + theme.toStdString() + "/about"))));
 	int precision;
 	double dblPrice = qstrPrice.toDouble();
 	string strCurrencyCode = currencyCode.toStdString();
