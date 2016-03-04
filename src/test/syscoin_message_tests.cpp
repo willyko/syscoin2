9d8efca7b81dfc70a821fd93c91d516427b4ed7b
sidhujag <sidhujag@hotmail.com>
2016-03-04 11:46:02 -0800

    fix message test

--- src/test/syscoin_message_tests.cpp
+++ src/test/syscoin_message_tests.cpp
@@ -36,7 +36,7 @@ BOOST_AUTO_TEST_CASE (generate_msgaliastransfer)
 {
 	MessageNew("node1", "node2", "title", "data", "jagmsg1", "jagmsg2");
 	// transfer an alias and send a message, the new node owner can now read messages to that alias
-	BOOST_CHECK_NO_THROW(CallRPC("node2", "aliasupdate jagmsg2 changeddata2 pvtdata jagmsg3"));
+	BOOST_CHECK_NO_THROW(AliasTransfer("node2", "jagmsg2", "node3", "changeddata2", "pvtdata"));
 	GenerateBlocks(1, "node2");
 	// send message to new node owning alias
 	MessageNew("node1", "node3", "title", "data", "jagmsg1", "jagmsg2");
