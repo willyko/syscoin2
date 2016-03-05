3755d83369a705dede94468e23f831335892e848
sidhujag <sidhujag@hotmail.com>
2016-03-04 21:31:05 -0800

    convert to value first

--- src/qt/offeracceptdialogbtc.cpp
+++ src/qt/offeracceptdialogbtc.cpp
@@ -50,7 +50,9 @@ OfferAcceptDialogBTC::OfferAcceptDialogBTC(const PlatformStyle *platformStyle, Q
 	info.message = GUIUtil::HtmlEscape(message);
 	info.address = this->address;
 	info.label = this->sellerAlias;
-	info.amount = AmountFromValue(ValueFromString(price.toStdString()));
+	UniValue value;
+    value.setNumStr(price.toStdString());
+	info.amount = AmountFromValue(value);
 	if (!platformStyle->getImagesOnButtons())
 	{
 		ui->confirmButton->setIcon(QIcon());
