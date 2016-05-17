dfb279dfa7aeb004d1fb18634f5d6635e9fc0de0
sidhujag <sidhujag@hotmail.com>
2016-05-16 22:46:36 -0700

    work on #73
    once wallet is unlocked, it remains unlocked until restart of wallet

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -637,7 +637,8 @@ WalletModel::UnlockContext WalletModel::requestUnlock()
     // If wallet is still locked, unlock was failed or cancelled, mark context as invalid
     bool valid = getEncryptionStatus() != Locked;
 
-    return UnlockContext(this, valid, was_locked);
+	// SYSCOIN
+    return UnlockContext(this, valid, false/*was_locked*/);
 }
 
 WalletModel::UnlockContext::UnlockContext(WalletModel *wallet, bool valid, bool relock):
