d5df67f4428bd4f80fa20f8d7b87ca55d4c6e571
sidhujag <sidhujag@hotmail.com>
2016-05-18 21:53:01 -0700

    typo

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -211,7 +211,7 @@ void OptionsDialog::setMapper()
 	mapper->addMapping(ui->defaultCertAlias, OptionsModel::DefaultCertAlias);
 	mapper->addMapping(ui->defaultPegAlias, OptionsModel::DefaultPegAlias);
 	mapper->addMapping(ui->defaultBuyOfferAlias, OptionsModel::DefaultBuyOfferAlias);
-	mapper->addMapping(ui->safesearch, OptionsModel::SafeSearch);
+	mapper->addMapping(ui->safeSearch, OptionsModel::SafeSearch);
     mapper->addMapping(ui->lang, OptionsModel::Language);
     mapper->addMapping(ui->unit, OptionsModel::DisplayUnit);
     mapper->addMapping(ui->thirdPartyTxUrls, OptionsModel::ThirdPartyTxUrls);
