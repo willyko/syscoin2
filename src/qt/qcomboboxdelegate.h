5010f8e8bcf62e2f7b8ce698bef2d79225180cbe
sidhujag <sidhujag@hotmail.com>
2016-05-19 23:28:54 -0700

    typo

--- src/qt/qcomboboxdelegate.h
+++ src/qt/qcomboboxdelegate.h
@@ -3,12 +3,15 @@
 
 #include <QItemDelegate>
 #include <QPainter>
-
+QT_BEGIN_NAMESPACE
+QT_END_NAMESPACE
 class ComboBoxDelegate : public QItemDelegate
 {
 Q_OBJECT
+
 public:
     explicit ComboBoxDelegate(QObject *parent = 0);
+	~ComboBoxDelegate();
 protected:
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
