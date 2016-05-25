68b308f13e127febcb8f3d73972f3e7981e73413
sidhujag <sidhujag@hotmail.com>
2016-05-24 21:59:15 -0700

    typo

--- src/qt/manageescrowdialog.cpp
+++ src/qt/manageescrowdialog.cpp
@@ -344,9 +344,9 @@ void ManageEscrowDialog::onLeaveFeedback()
 	UniValue params(UniValue::VARR);
 	string strMethod = string("escrowfeedback");
 	params.push_back(escrow.toStdString());
-	params.push_back(QString::number(ui->primaryRating->value).toStdString());
+	params.push_back(QString::number(ui->primaryRating->value()).toStdString());
 	params.push_back(ui->primaryFeedback->toPlainText().toStdString());
-	params.push_back(QString::number(ui->secondaryRating->value).toStdString());
+	params.push_back(QString::number(ui->secondaryRating->value()).toStdString());
 	params.push_back(ui->secondaryFeedback->toPlainText().toStdString());
 	try {
 		UniValue result = tableRPC.execute(strMethod, params);
