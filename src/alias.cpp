4e5a9309b4ef5fd39a1b950f5ac208c664ceed3c
sidhujag <sidhujag@hotmail.com>
2016-06-24 10:18:41 -0700

    fix alias null check

--- src/alias.cpp
+++ src/alias.cpp
@@ -835,11 +835,13 @@ bool CheckAliasInputs(const CTransaction &tx, int op, int nOut, const vector<vec
 				// Check name
 				if (vvchPrevArgs[0] != vvchArgs[0])
 					return error("CheckAliasInputs() : aliasupdate alias mismatch");
-				if (!paliasdb->ReadAlias(vvchArgs[0], vtxPos) || vtxPos.empty())
-					return error("CheckAliasInputs() : failed to read from alias DB");
-				if((vtxPos.back().nHeight + GetAliasExpirationDepth()) < nHeight && vvchArgs[0] != vchFromString("SYS_BAN") && vvchArgs[0] != vchFromString("SYS_RATES") && vvchArgs[0] != vchFromString("SYS_CATEGORY"))
-					return error("CheckAliasInputs(): Trying to update an expired service");
-				
+				if(!theAlias.IsNull())
+				{
+					if (!paliasdb->ReadAlias(vvchArgs[0], vtxPos) || vtxPos.empty())
+						return error("CheckAliasInputs() : failed to read from alias DB");
+					if((vtxPos.back().nHeight + GetAliasExpirationDepth()) < nHeight && vvchArgs[0] != vchFromString("SYS_BAN") && vvchArgs[0] != vchFromString("SYS_RATES") && vvchArgs[0] != vchFromString("SYS_CATEGORY"))
+						return error("CheckAliasInputs(): Trying to update an expired service");
+				}
 				break;
 		default:
 			return error(
