952da4eb005fc1ccb847f88b29c052c389e77cac
sidhujag <sidhujag@hotmail.com>
2016-06-26 14:11:52 -0700

    update test

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -291,6 +291,8 @@ BOOST_AUTO_TEST_CASE (generate_aliasexpired)
 		MilliSleep(2500);
 		BOOST_CHECK_NO_THROW(CallRPC("node3", "generate 5"));
 		MilliSleep(2500);
+		BOOST_CHECK_NO_THROW(CallRPC("node2", "generate 5"));
+		MilliSleep(2500);
 		// ensure node3 can see (not pruned) expired escrows that aren't complete or refunded yet
 		BOOST_CHECK_NO_THROW(CallRPC("node3", "escrowinfo " + escrowguid));
 		// and node2
