3e9e67f300b7a33b051058040de5825f4c456283
sidhujag <sidhujag@hotmail.com>
2016-06-27 16:22:14 -0700

    typo

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -427,7 +427,7 @@ BOOST_AUTO_TEST_CASE (generate_aliasexpired)
 		BOOST_CHECK_NO_THROW(CallRPC("node2", "escrowinfo " + escrowguid));
 		// able to release and claim release on escrow with expired aliases and expired escrow (not complete or refunded)
 		EscrowRelease("node2", escrowguid);	 
-		EscrowClaimRelease("node1", escrowguid);
+		EscrowClaimRelease("node1", escrowguid); 
 
 		// should fail: update cert with expired alias
 		BOOST_CHECK_THROW(CallRPC("node1", "certupdate " + certguid + " jag1 data 0"), runtime_error);
