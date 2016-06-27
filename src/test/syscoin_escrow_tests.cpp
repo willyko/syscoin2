2b0b41800154973f2b8b6dae2a8342c97ae95c82
sidhujag <sidhujag@hotmail.com>
2016-06-26 21:34:01 -0700

    fix escrow test

--- src/test/syscoin_escrow_tests.cpp
+++ src/test/syscoin_escrow_tests.cpp
@@ -243,6 +243,8 @@ BOOST_AUTO_TEST_CASE (generate_escrowpruning)
 		MilliSleep(2500);
 		BOOST_CHECK_NO_THROW(CallRPC("node1", "aliasupdate selleraliasprune newdata privdata"));
 		BOOST_CHECK_NO_THROW(CallRPC("node2", "aliasupdate buyeraliasprune newdata privdata"));
+		BOOST_CHECK_NO_THROW(CallRPC("node2", "generate 2"));
+		MilliSleep(2500);
 		// stop and start node1
 		StopNode("node1");
 		StartNode("node1");
