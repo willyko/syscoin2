777371311457dc970241169936c7e8c7b19dd57c
sidhujag <sidhujag@hotmail.com>
2016-07-07 13:06:23 -0700

    typo

--- src/test/test_syscoin_services.cpp
+++ src/test/test_syscoin_services.cpp
@@ -970,11 +970,12 @@ const UniValue FindOfferAcceptFeedback(const string& node, const string& offergu
 	UniValue r, ret;
 	BOOST_CHECK_NO_THROW(r = CallRPC(node, "offerinfo " + offerguid));
 	const UniValue &arrayValue = find_value(r.get_obj(), "accepts").get_array();
+	const string &offervalueguid = find_value(r.get_obj(), "offer").get_str();
 	for(int i=0;i<arrayValue.size();i++)
 	{
 		const UniValue& arrayObj = arrayValue[i].get_obj();
 		const string &acceptvalueguid = find_value(arrayObj, "id").get_str();
-		const string &offervalueguid = find_value(arrayObj, "offer").get_str();
+		
 		if(acceptvalueguid == acceptguid && offervalueguid == offerguid)
 		{
 			const UniValue &arrayBuyerFeedbackValue = arrayBuyerFeedbackValue[i].get_obj();
