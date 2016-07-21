61775acbf8cfd83e27dfb12a9c8735a8b323937f
sidhujag <sidhujag@hotmail.com>
2016-07-20 23:03:32 -0700

    typo

--- src/qt/certview.cpp
+++ src/qt/certview.cpp
@@ -26,8 +26,8 @@ CertView::CertView(const PlatformStyle *platformStyle, QStackedWidget *parent):
     walletModel(0)
 {
 	tabWidget = new QTabWidget();
-    certListPage = new CertListPage(platformStyle,this);
-    myCertListPage = new MyCertListPage(platformStyle, this);
+    certListPage = new CertListPage(platformStyle,parent);
+    myCertListPage = new MyCertListPage(platformStyle, parent);
 	QString theme = GUIUtil::getThemeName();
 	tabWidget->addTab(myCertListPage, tr("&My Certificates"));
 	tabWidget->addTab(certListPage, tr("&Search"));
