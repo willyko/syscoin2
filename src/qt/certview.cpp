d09d4d5ad06f7234b382a6fa1981dd2492196718
sidhujag <sidhujag@hotmail.com>
2016-07-20 23:00:58 -0700

    set parent of cert pages

--- src/qt/certview.cpp
+++ src/qt/certview.cpp
@@ -26,8 +26,8 @@ CertView::CertView(const PlatformStyle *platformStyle, QStackedWidget *parent):
     walletModel(0)
 {
 	tabWidget = new QTabWidget();
-    certListPage = new CertListPage(platformStyle);
-    myCertListPage = new MyCertListPage(platformStyle);
+    certListPage = new CertListPage(platformStyle,this);
+    myCertListPage = new MyCertListPage(platformStyle, this);
 	QString theme = GUIUtil::getThemeName();
 	tabWidget->addTab(myCertListPage, tr("&My Certificates"));
 	tabWidget->addTab(certListPage, tr("&Search"));
